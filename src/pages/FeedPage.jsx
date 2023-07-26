33f472b77c9308609d0d7dd7cbf7910ed19d3f3e
eunseong7207 <les070207@naver.com>
2023-07-26 16:23:56 +0900

    ⚙️ :: (#6) 기능을 제외한 피드 페이지 세팅
    일부 기능을 제외하고 만든 피드 페이지입니다.

--- src/pages/FeedPage.jsx
+++ src/pages/FeedPage.jsx
@@ -2,8 +2,17 @@ import { styled } from "styled-components";
 import { Logo } from "../assets";
 import { useState, useRef } from "react";
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 const FeedPage = () => {
+  const navigate = useNavigate();
+
+  const onInputHandler = (e) => {
+    setInputCount(e.target.value.length);
+  };
+
+  let [inputCount, setInputCount] = useState(0);
+
   const [Image, setImage] = useState(
     "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png"
   );
@@ -52,6 +61,32 @@ const FeedPage = () => {
           ref={fileInput}
         />
       </AvatarContainer>
+      <InputContainer>
+        <Input
+          onChange={onInputHandler}
+          maxLength="10"
+          placeholder="위치를 입력하세요"
+        />
+        <p>
+          <span>{inputCount}</span>
+          <span>/10</span>
+        </p>
+        <Txt>태그는 최대 6개, 최대 10자까지 작성 가능합니다.</Txt>
+        <PlusBtnContainer>
+          <button>+</button>
+        </PlusBtnContainer>
+      </InputContainer>
+      <UploadTxt>어느 그룹에 업로드 하시겠습니까?</UploadTxt>
+      <Group>
+        <GroupBtn>2023</GroupBtn>
+      </Group>
+      <BottomBtnContainer>
+        <CancelBtn>취소하기</CancelBtn>
+        <UploadBtn>업로드</UploadBtn>
+      </BottomBtnContainer>
+      <LocateMypage>
+        <button onClick={() => navigate("/mypage")}>마이페이지</button>
+      </LocateMypage>
     </Container>
   );
 };
@@ -62,7 +97,8 @@ const Container = styled.div`
   background-color: ${({ theme }) => theme.colors.gray0};
   display: flex;
   flex-direction: column;
-  gap: 50px;
+
+  /* gap: 50px; */
 `;
 
 const LogoContainer = styled.div`
@@ -86,8 +122,110 @@ const AvatarContainer = styled.div`
 `;
 
 const Avatar = styled.img`
-  width: 250px;
-  height: 250px;
+  width: 200px;
+  height: 200px;
+  border-radius: 8px;
+`;
+
+const Input = styled.input`
+  width: 550px;
+  height: 50px;
+  border-radius: 10px;
+  outline: none;
+  border: 1px solid ${({ theme }) => theme.gray400};
+  font-size: 20px;
+  border-color: rgb(239, 204, 202);
+  padding-left: 10px;
+`;
+
+const InputContainer = styled.div`
+  padding-left: 475px;
+  padding-top: 30px;
+  > p {
+    padding-left: 500px;
+  }
+`;
+
+const Txt = styled.text`
+  color: red;
+  margin-left: 0px;
+  margin-bottom: 100px;
+`;
+
+const PlusBtnContainer = styled.div`
+  padding-left: 0px;
+  padding-top: 20px;
+  > button {
+    width: 90px;
+    height: 60px;
+    border-radius: 24px;
+    border: 1px solid;
+    border-color: rgb(239, 204, 202);
+    color: black;
+    font-size: 20px;
+    background-color: white;
+  }
+`;
+
+const Group = styled.div`
+  padding-left: 470px;
+  padding-top: 30px;
+`;
+const GroupBtn = styled.button`
+  border: 1px solid;
+  border-color: rgb(239, 204, 202);
+  color: gray;
+  border-radius: 24px;
+  width: 150px;
+  height: 50px;
+  font-size: 28px;
+  background-color: white;
+`;
+
+const UploadTxt = styled.text`
+  padding-left: 475px;
+  padding-top: 100px;
+`;
+
+const BottomBtnContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  /* margin-top: 100px; */
+  padding-top: 70px;
+  gap: 60px;
+`;
+
+const CancelBtn = styled.button`
+  width: 290px;
+  height: 40px;
+  background-color: white;
+  border: 1px solid;
+  border-color: rgb(239, 204, 202);
+  color: gray;
+  border-radius: 8px;
+  font-size: 20px;
+`;
+
+const UploadBtn = styled.button`
+  border: none;
+  background-color: rgb(239, 204, 202);
+  color: gray;
+  width: 290px;
+  height: 40px;
+  border-radius: 8px;
+  font-size: 20px;
+`;
+
+const LocateMypage = styled.div`
+  padding-left: 50px;
+  padding-bottom: 0px;
+  > button {
+    color: gray;
+    font-size: 20px;
+    background: none;
+    border: none;
+  }
 `;
 
 export default FeedPage;
