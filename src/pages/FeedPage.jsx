74b8d9f17fa7815db6db4901d05254bcf2d28650
eunseong7207 <les070207@naver.com>
2023-07-26 16:19:20 +0900

    ⚙️ :: (#6) 피드 페이지 세팅
    기본적인 피드 페이지를 세팅했습니다.

--- src/pages/FeedPage.jsx
+++ src/pages/FeedPage.jsx
@@ -3,7 +3,7 @@ import { Logo } from "../assets";
 import { useState, useRef } from "react";
 import React from "react";
 
-const MyPage = () => {
+const FeedPage = () => {
   const [Image, setImage] = useState(
     "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png"
   );
@@ -29,14 +29,11 @@ const MyPage = () => {
     };
     reader.readAsDataURL(e.target.files[0]);
   };
-
   return (
     <Container>
       <LogoContainer>
         <img src={Logo} alt="로고" />
       </LogoContainer>
-      <MyPageTxT>마이 페이지</MyPageTxT>
-      <Txt>OO님, 오늘은 어떤 하루를 보냈나요?</Txt>
       <AvatarContainer>
         <Avatar src={Image} size={200} />
         <p
@@ -44,7 +41,7 @@ const MyPage = () => {
             fileInput.current.click();
           }}
         >
-          수정하기
+          사진 추가하기
         </p>
         <input
           type="file"
@@ -55,14 +52,6 @@ const MyPage = () => {
           ref={fileInput}
         />
       </AvatarContainer>
-      <IdContainer>
-        <input type="text" />
-        <CheckBtn>중복확인</CheckBtn>
-      </IdContainer>
-      <BottomBtnContainer>
-        <CancelBtn>취소하기</CancelBtn>
-        <CompleteBtn>완료</CompleteBtn>
-      </BottomBtnContainer>
     </Container>
   );
 };
@@ -86,20 +75,6 @@ const LogoContainer = styled.div`
   }
 `;
 
-const MyPageTxT = styled.span`
-  padding-left: 400px;
-  font-size: 16px;
-`;
-
-const Avatar = styled.img`
-  width: 200px;
-  height: 200px;
-  border: none;
-  border-radius: 50%;
-  align-content: center;
-  margin: 20px;
-`;
-
 const AvatarContainer = styled.div`
   display: flex;
   flex-direction: column;
@@ -110,61 +85,9 @@ const AvatarContainer = styled.div`
   }
 `;
 
-const CancelBtn = styled.button`
-  border-radius: 8px;
-  width: 300px;
-  border: 1px;
-  height: 50px;
-  border-color: black;
+const Avatar = styled.img`
+  width: 250px;
+  height: 250px;
 `;
 
-const CompleteBtn = styled.button`
-  border-color: #50574b;
-  background-color: #50574b;
-  color: white;
-  border-radius: 8px;
-  border: 1px;
-  width: 300px;
-  height: 50px;
-`;
-
-const BottomBtnContainer = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  margin-top: 100px;
-  gap: 60px;
-`;
-
-const Txt = styled.p`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  font-size: 20px;
-`;
-
-const IdContainer = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  gap: 10px;
-  > input {
-    width: 340px;
-    height: 40px;
-    border-radius: 0.625rem;
-    border: 1px solid ${({ theme }) => theme.gray400};
-    outline: none;
-    padding: 0 20px;
-  }
-`;
-
-const CheckBtn = styled.button`
-  width: 100px;
-  height: 40px;
-  background-color: #50574b;
-  border-radius: 10px;
-  color: white;
-  border: none;
-`;
-
-export default MyPage;
+export default FeedPage;
