7b528a8e06b14c9a5d63e2f0378dabb343edd8f4
inung1004 <dksdbswl10430@naver.com>
2023-08-20 23:01:45 +0900

    ⚡️ :: QueryClientProvider 추가

--- src/App.js
+++ src/App.js
@@ -3,15 +3,20 @@ import { BrowserRouter } from "react-router-dom";
 import { theme } from "./style/Theme";
 import { ThemeProvider } from "styled-components";
 import GlobalStyle from "./style/GlobalStyle";
+import { QueryClient, QueryClientProvider } from "react-query";
 
 function App() {
+  const queryClient = new QueryClient();
+
   return (
-    <ThemeProvider theme={theme}>
-      <GlobalStyle />
-      <BrowserRouter>
-        <MainRouter />
-      </BrowserRouter>
-    </ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <BrowserRouter>
+          <MainRouter />
+        </BrowserRouter>
+      </ThemeProvider>
+    </QueryClientProvider>
   );
 }
 
